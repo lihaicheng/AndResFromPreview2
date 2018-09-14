2a336f50ad8d9bd6cfef2fd70a28b681d7f22f80
H.C. Lee <lihaicheng_work@163.com>
2018-09-14 16:31:50 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AndResFromPreview2
---
+++ app/src/main/java/com/resource/hc/andresfrompreview2/Lagest.java
@@ -0,0 +1,7 @@
+package com.resource.hc.andresfrompreview2;
+
+public class Lagest
+{
+    public static int largestgetWidth;
+    public static int largestgetHeight;
+}
